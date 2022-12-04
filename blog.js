c5e47ccfca740f917030606bfc7e78e6e68ffc64
aganmarw <97824022+aganmarw@users.noreply.github.com>
2022-12-04 13:37:53 +0700

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName("title")[0].remove();
+    const ishead = document.getElementsByTagName("head")[0];
+    let meta = "<title>Agcv2 Contractflooringmagazine Com Bloggerjs Js</title><meta name=\"description\" content=\"Search your article on Agcv2 Contractflooringmagazine Com Bloggerjs Js - \" />";
+    ishead.insertAdjacentHTML("beforeend", meta);
+    const ispost = document.getElementsByClassName("slot_header")[0];
+    let pozt = "<div class=\"row\"><div class=\"col-md-12\"><ul class=\"list-group\"><li class=\"list-group-item d-flex justify-content-between align-items-center\"><a href=\"///search/q=mission%20counter%20strike%201%208%20apk%20download\" title=\"Mission Counter Strike 1.8 APK Download\" style=\"font-weight:700;color:#6c6c6c;text-decoration:none;\">Mission Counter Strike 1.8 APK Download</a></li></ul></div></div>";
+    ispost.insertAdjacentHTML("afterend", pozt);
